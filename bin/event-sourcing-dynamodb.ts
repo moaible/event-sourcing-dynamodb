2068cf9db1d59a499b1bc807c29e2158f69c3428
moaible <moai.motodera@gmail.com>
2021-11-11 05:04:17 +0900

    Update owner name

--- bin/event-sourcing-dynamodb.ts
+++ bin/event-sourcing-dynamodb.ts
@@ -5,4 +5,4 @@ import { EventSourcingDynamodbStack } from '../lib/event-sourcing-dynamodb-stack
 
 const app = new cdk.App();
 const stack = new EventSourcingDynamodbStack(app, 'EventSourcingDynamodbStack');
-cdk.Tag.add(stack, 'ippon:owner', 'tlebrun');
+cdk.Tag.add(stack, 'owner', 'moaible');
